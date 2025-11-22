0514d2a62879d2b61480a44ef1effed5b81ff537
Anner Escobar <anner123escobar@gmail.com>
2025-11-21 21:04:08 -0600

    corregida la url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://pos-backend-production-e832.up.railway.app', // tu backend local
+  apiUrl: 'https://pos-backend-production-e832.up.railway.app', // tu backend local
 };
